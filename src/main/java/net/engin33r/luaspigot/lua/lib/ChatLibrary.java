f939e03f723a772cbe79132ca579f417da5af30e
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-04-14 14:21:47 +0300

    Use new annotation for chat library

--- src/main/java/net/engin33r/luaspigot/lua/lib/ChatLibrary.java
+++ src/main/java/net/engin33r/luaspigot/lua/lib/ChatLibrary.java
@@ -1,7 +1,7 @@
 package net.engin33r.luaspigot.lua.lib;
 
-import net.engin33r.luaspigot.lua.Function;
 import net.engin33r.luaspigot.lua.Library;
+import net.engin33r.luaspigot.lua.annotation.LibFunctionDef;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.luaj.vm2.Varargs;
@@ -12,38 +12,25 @@ import org.luaj.vm2.LuaValue;
  */
 @SuppressWarnings("unused")
 public class ChatLibrary extends Library {
-    public ChatLibrary() {
-        registerFunction(new BroadcastFunction());
-    }
-
     @Override
     public String getName() {
         return "chat";
     }
 
-    private class BroadcastFunction extends Function {
-        @Override
-        public String getName() {
-            return "broadcast";
+    @LibFunctionDef(name = "broadcast")
+    public Varargs broadcast(Varargs args) {
+        int n = args.narg();
+        if (n == 0) return LuaValue.NIL;
+
+        String str = "";
+        for (int i = 1; i <= args.narg(); i++) {
+            if (i > 1) str += "\t";
+            str += args.checkjstring(i);
         }
 
-        @Override
-        public Varargs call(Varargs args) {
-            int n = args.narg();
-            if (n == 0) return LuaValue.NIL;
-
-            String str = "";
-            for (int i = 1; i <= args.narg(); i++) {
-                if (i > 1) str += "\t";
-                str += args.checkjstring(i);
-            }
-
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                p.sendMessage(str);
-            }
-            return LuaValue.NIL;
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.sendMessage(str);
         }
+        return LuaValue.NIL;
     }
-
-
 }
