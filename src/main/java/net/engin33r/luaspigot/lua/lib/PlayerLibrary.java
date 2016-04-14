5d2a98f6b481c49f218df85a416f8fede9617e42
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-04-14 14:22:46 +0300

    Add WIP player library

---
+++ src/main/java/net/engin33r/luaspigot/lua/lib/PlayerLibrary.java
@@ -0,0 +1,25 @@
+package net.engin33r.luaspigot.lua.lib;
+
+import net.engin33r.luaspigot.lua.Library;
+import net.engin33r.luaspigot.lua.annotation.LibFunctionDef;
+import net.engin33r.luaspigot.lua.type.LuaPlayer;
+import org.bukkit.Bukkit;
+import org.luaj.vm2.Varargs;
+
+/**
+ * Library for interacting with players on and off the server.
+ * Mostly deals with creating {@link LuaPlayer} instances.
+ * @see LuaPlayer
+ */
+@SuppressWarnings("unused")
+public class PlayerLibrary extends Library {
+    @Override
+    public String getName() {
+        return "player";
+    }
+
+    @LibFunctionDef(name = "getByName")
+    public Varargs getByName(Varargs args) {
+        return new LuaPlayer(Bukkit.getOfflinePlayer(args.checkjstring(1)));
+    }
+}
