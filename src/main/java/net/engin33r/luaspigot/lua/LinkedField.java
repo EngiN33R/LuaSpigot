058ba31cc60df72d5ea72a35c0ecd9b5c4b6e630
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-04-16 16:57:08 +0300

    Add linked field implementation

---
+++ src/main/java/net/engin33r/luaspigot/lua/LinkedField.java
@@ -0,0 +1,18 @@
+package net.engin33r.luaspigot.lua;
+
+import org.luaj.vm2.LuaValue;
+
+/**
+ * Abstract class defining a linked field - technically a table field in Lua
+ * that updates every time it's queried for and performs an action whenever
+ * it is updated.
+ */
+public abstract class LinkedField<T extends WeakType> extends DynamicField {
+    public LinkedField(WeakType self) { super(self); }
+
+    public abstract String getName();
+
+    public abstract void update(LuaValue val);
+
+    public abstract LuaValue query();
+}
