a4d773ef75f746de88f9f8c152074c04920e4478
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-05-21 20:32:01 +0300

    Add more useful Bukkit functions

--- LuaSpigot-core/src/main/java/net/engin33r/luaspigot/lua/lib/BukkitUtilsLibrary.java
+++ LuaSpigot-core/src/main/java/net/engin33r/luaspigot/lua/lib/BukkitUtilsLibrary.java
@@ -3,17 +3,23 @@ package net.engin33r.luaspigot.lua.lib;
 import net.engin33r.luaspigot.lua.Library;
 import net.engin33r.luaspigot.lua.TypeValidator;
 import net.engin33r.luaspigot.lua.annotation.LibFunctionDef;
+import net.engin33r.luaspigot.lua.type.LuaBlock;
 import net.engin33r.luaspigot.lua.type.LuaItem;
 import net.engin33r.luaspigot.lua.type.LuaPlayer;
+import net.engin33r.luaspigot.lua.type.LuaWorld;
 import net.engin33r.luaspigot.lua.type.util.LuaColor;
 import net.engin33r.luaspigot.lua.type.util.LuaUUID;
 import net.engin33r.luaspigot.lua.type.util.LuaVector;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 
 import java.util.UUID;
+
+import static org.luaj.vm2.LuaValue.NIL;
 
 /**
  * General-purpose utility library for creating and discovering Spigot objects
@@ -41,6 +47,27 @@ public class BukkitUtilsLibrary extends Library {
         return new LuaItem(args.checkjstring(1), args.optint(2, 1));
     }
 
+    @LibFunctionDef(name = "at", module = "block")
+    public Varargs blockAt(Varargs args) {
+        TypeValidator.validate(args.checktable(1), "world");
+        return new LuaBlock(new Location(((LuaWorld) args.checktable(1))
+                .getHandle(), args.checkdouble(2), args.checkdouble(3),
+                args.checkdouble(4)));
+    }
+
+    @LibFunctionDef(name = "getByName", module = "world")
+    public Varargs getWorldByName(Varargs args) {
+        World w = Bukkit.getWorld(args.checkjstring(1));
+        return w == null ? NIL : new LuaWorld(w);
+    }
+
+    @LibFunctionDef(name = "getByUUID", module = "world")
+    public Varargs getWorldByUUID(Varargs args) {
+        TypeValidator.validate(args.checktable(1), "uuid");
+        World w = Bukkit.getWorld(((LuaUUID) args.checktable(1)).getHandle());
+        return w == null ? NIL : new LuaWorld(w);
+    }
+
     @LibFunctionDef(name = "getByName", module = "player")
     @SuppressWarnings("deprecation")
     public Varargs getPlayerByName(Varargs args) {
@@ -63,7 +90,7 @@ public class BukkitUtilsLibrary extends Library {
         } else {
             LuaValue.error("string or table expected, got "
                     + args.arg(1).type());
-            return LuaValue.NIL;
+            return NIL;
         }
 
         return new LuaPlayer(Bukkit.getOfflinePlayer(uuid));
@@ -72,7 +99,7 @@ public class BukkitUtilsLibrary extends Library {
     @LibFunctionDef(name = "broadcast", module = "chat")
     public Varargs broadcast(Varargs args) {
         int n = args.narg();
-        if (n == 0) return LuaValue.NIL;
+        if (n == 0) return NIL;
 
         String str = "";
         for (int i = 1; i <= args.narg(); i++) {
@@ -83,6 +110,6 @@ public class BukkitUtilsLibrary extends Library {
         for (Player p : Bukkit.getOnlinePlayers()) {
             p.sendMessage(str);
         }
-        return LuaValue.NIL;
+        return NIL;
     }
 }
